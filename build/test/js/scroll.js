2685e27d49ce5c576ae56df1e49a9540e181696e
chocolate-holding-company <how5360@gmail.com>
2026-01-29 16:58:38 +0000

    changed picture, removed scroll script

--- build/test/js/scroll.js
+++ build/test/js/scroll.js
@@ -1,25 +1,25 @@
 //scroll
 
-function smoothScroll(target) {
- const element = document.getElementById(target);
- if (element) {
-  window.scrollTo({
-   top: element.offsetTop,
-   behavior: "smooth",
-  });
- }
-}
+// function smoothScroll(target) {
+//  const element = document.getElementById(target);
+//  if (element) {
+//   window.scrollTo({
+//    top: element.offsetTop,
+//    behavior: "smooth",
+//   });
+//  }
+// }
 
-document.addEventListener("DOMContentLoaded", function () {
- const anchors = document.querySelectorAll('a[href^="#"]');
- anchors.forEach(function (anchor) {
-  anchor.addEventListener("click", function (event) {
-   event.preventDefault();
-   const target = anchor.getAttribute("href").substring(1);
-   smoothScroll(target);
-  });
- });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//  const anchors = document.querySelectorAll('a[href^="#"]');
+//  anchors.forEach(function (anchor) {
+//   anchor.addEventListener("click", function (event) {
+//    event.preventDefault();
+//    const target = anchor.getAttribute("href").substring(1);
+//    smoothScroll(target);
+//   });
+//  });
+// });
 
 //show/hide the back button
 const backButton = document.getElementById("topBtn");
